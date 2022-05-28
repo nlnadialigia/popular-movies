40d98beb48f25398aac53b88c1b5b72c36092c62
Nadia Ligia <nlnadialigia@gmail.com>
2022-05-28 13:25:40 -0300

    feat($api): consumindo a api

--- src/scripts/movies.js
+++ src/scripts/movies.js
@@ -1,45 +1,41 @@
-const movies = document.querySelector("#movies")
-const moviesList = [
-  {
-    image: 'https://img.elo7.com.br/product/original/3FBA809/big-poster-filme-batman-2022-90x60-cm-lo002-poster-batman.jpg',
-    title: 'Batman',
-    rating: 9.2,
-    year: 2022,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    isFavorited: true,
-  },
-  {
-    image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/9/9b/Avengers_Endgame.jpg/250px-Avengers_Endgame.jpg',
-    title: 'Avengers',
-    rating: 9.2,
-    year: 2019,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    isFavorited: false
-  },
-  {
-    image: 'https://upload.wikimedia.org/wikipedia/en/1/17/Doctor_Strange_in_the_Multiverse_of_Madness_poster.jpg',
-    title: 'Doctor Strange',
-    rating: 9.2,
-    year: 2022,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    isFavorited: false
-  },
-]
+import {apiKey, apiToken} from './tmdb.config.js'
 
-function renderMovies() {
+const movies = document.querySelector("#movies")
+
+const baseURL = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
+
+const moviesData = async () => {
+  try {
+    const response = await fetch(baseURL, { 
+      headers: new Headers ({
+        Authorization: `Bearer ${apiToken}`
+      }),
+      mode: "cors"
+    })
+    let data = await response.json()
+    data = data.results
+    
+    return data
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function renderMovies() {
+  const moviesList = await moviesData()
   moviesList.forEach(movie => {
-    console.log(movie);
+    const imageURL = `https://image.tmdb.org/t/p/original${movie.poster_path}`
     movies.innerHTML += `
       <div class="movie">
-        <img class="logo" src=${movie.image}>
+        <img class="logo" src=${imageURL}>
         <div id="title">
           <p class="title">${movie.title}</p>
           <div class="icons">
-            <p><img src="./assets/full-star.svg">${movie.rating}</p>
+            <p><img src="./src/assets/full-star.svg">${movie.vote_average}</p>
             <p><img src=${favorite(movie.isFavorited)}>Favoritar</p>
           </div>
         </div>
-        <p class="description">${movie.description}</p>
+        <p class="description">${movie.overview}</p>
      </div>
     `
   })
@@ -47,10 +43,11 @@ function renderMovies() {
 
 function favorite(type) {
   if (type) {
-    return `./assets/full-heart.svg`
+    return `./src/assets/full-heart.svg`
   } else {
-    return `./assets/heart.svg`
+    return `./src/assets/heart.svg`
   }
 }
 
+window.onload = renderMovies
 
